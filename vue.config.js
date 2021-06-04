02d3ea711a520d906277197353c063be8ee1b152
fumisan <fumitaka.murakami@levelenter.com>
2021-06-04 15:22:57 +0900

    com

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,12 @@ module.exports = {
   publicPath:
     process.env.NODE_ENV === "production" ? "/levelenter-garally/" : "/",
 };
+// vue-cli-app
+module.exports = {
+  publicPath: "/vue-cli-app",
+};
+
+// vue-cli-ts-app
+module.exports = {
+  publicPath: "/vue-cli-ts-app",
+};
